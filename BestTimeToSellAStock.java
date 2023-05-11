3ab9e7e861b38b741d4595b929bd137637c25306
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-05-11 21:16:10 +0100

    Create BestTimeToSellAStock.java

---
+++ BestTimeToSellAStock.java
@@ -0,0 +1,15 @@
+//Time Complexity is O(N) and Space Complexity is O(1)
+
+public class Solution {
+    public int maxProfit(int prices[]) {
+        int maxprofit = 0;
+        for (int i = 0; i < prices.length - 1; i++) {
+            for (int j = i + 1; j < prices.length; j++) {
+                int profit = prices[j] - prices[i];
+                if (profit > maxprofit)
+                    maxprofit = profit;
+            }
+        }
+        return maxprofit;
+    }
+}
