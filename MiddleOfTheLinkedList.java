1e2f1cd52ce53ecb66e36651c70ee70997b8adbc
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-05-19 23:39:54 +0100

    Create MiddleOfTheLinkedList.java

---
+++ MiddleOfTheLinkedList.java
@@ -0,0 +1,12 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
