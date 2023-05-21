102cb89690cf6198c2460a45c3866526cc33b321
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-05-21 22:51:36 +0100

    Create PivotIndex.java

---
+++ PivotIndex.java
@@ -0,0 +1,23 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int sum = 0;
+        int leftSum = 0;
+
+        for(int num: nums){
+            sum+=num;
+        }
+
+        for(int i =0; i < nums.length; i++){
+            if(leftSum == sum -leftSum-nums[i]){
+                return i;
+            }
+        leftSum+= nums[i];
+        }
+    return -1;
+
+       
+    }
+}
