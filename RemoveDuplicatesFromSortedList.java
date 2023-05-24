2ca7ddd281aafc9436d27f8e6b3fa7fdaa30b378
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-05-24 22:01:34 +0100

    Create RemoveDuplicatesFromSortedList.java

---
+++ RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,19 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode current = head;
+        while(current!=null && current.next!=null){
+            if(current.next.val== current.val){
+                current.next=current.next.next;
+            } else{
+                current=current.next;
+            }
+
+        }
+        return head;
+
+        
+    }
+}
