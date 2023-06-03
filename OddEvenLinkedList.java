848d7df0dbdba6e82125f6b965b6747a8456d4ba
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-06-03 21:23:38 +0100

    Create OddEvenLinkedList.java

---
+++ OddEvenLinkedList.java
@@ -0,0 +1,16 @@
+//Time Complexity: O(N)
+//Space Complexity: O(1)
+public class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null) return null;
+        ListNode odd = head, even = head.next, evenHead = even;
+        while (even != null && even.next != null) {
+            odd.next = even.next;
+            odd = odd.next;
+            even.next = odd.next;
+            even = even.next;
+        }
+        odd.next = evenHead;
+        return head;
+    }
+}
