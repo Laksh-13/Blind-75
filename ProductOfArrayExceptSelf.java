f4ea0a9fad949579a80d438d69b9f461d7e36f5e
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-06-15 10:28:31 +0100

    Update ProductOfArrayExceptSelf.java

--- ProductOfArrayExceptSelf.java
+++ ProductOfArrayExceptSelf.java
@@ -25,3 +25,26 @@ class Solution {
         return output;
     }
 }
+
+
+//Space: O(1) considering the o/p array does not contribute to the space complexity. I m updating the right prod array on the fly
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+
+        int length = nums.length;
+        int[] output = new int[length]; //would contain the prod of all elements to the right
+
+        output[0] = 1;
+        for (int i = 1; i < length; i++) {
+            output[i] = nums[i - 1] * output[i - 1];
+        }
+
+        int rightProd = 1;
+        for (int i = length - 1; i >= 0; i--) {
+            output[i] = output[i] * rightProd;
+            rightProd *= nums[i];
+        }
+        
+        return output;
+    }
+}
