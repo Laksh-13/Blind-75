81efe189a1e9d207746d8c86cf63e335439710a7
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-06-21 06:54:55 +0100

    Update DuplicateElement.java

--- DuplicateElement.java
+++ DuplicateElement.java
@@ -1,7 +1,13 @@
+//Time Complexity: O(N) as we do search and insert n times which is const time operation
+//Space Complexity: O(N)
+
+class Solution{
 public boolean containsDuplicate(int[] nums) {
-    Arrays.sort(nums);
-    for (int i = 0; i < nums.length - 1; ++i) {
-        if (nums[i] == nums[i + 1]) return true;
+    Set<Integer> unique = new HashSet<>();
+    for (int i = 0; i < nums.length; i++) {
+        if (unique.contains(nums[i])) return true;
+        unique.add(nums[i]);
     }
     return false;
+}
 }
