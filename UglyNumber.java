d08b68960c665f4be92291625f21b9a55ac36e12
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-06-25 22:59:33 +0100

    Create UglyNumber.java

---
+++ UglyNumber.java
@@ -0,0 +1,13 @@
+//Time Complexity: O(log N) --->when dividing an integer x by y, there can be at most log x (base y) ops. So atmost log N(base 2) + log N(base 3) + log N(base 5) i.e log N complexity
+//Space: O(1)
+class Solution {
+    public boolean isUgly(int n) {
+        if(n <= 0)
+            return false;
+        
+        for(int factor: new int[] {2, 3, 5})
+            while(n % factor == 0)
+                n = n / factor;
+        return n == 1;
+    }
+}
