12d8d447b56dae1e22775e2f4073e8d05a623055
Lakshika Juneja <44654646+Laksh-13@users.noreply.github.com>
2023-06-27 22:30:07 +0100

    Create AllMissingNumbers.java

---
+++ AllMissingNumbers.java
@@ -0,0 +1,24 @@
+//Time Compleixty: O(N)
+//Space Complexity: O(N)
+
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+
+         HashMap<Integer, Boolean> hashTable = new HashMap<Integer, Boolean>();
+        
+        for (int i = 0; i < nums.length; i++) {
+            hashTable.put(nums[i], true);
+        }
+        
+        List<Integer> result = new LinkedList<Integer>();
+        
+        for (int i = 1; i <= nums.length; i++) {
+            if (!hashTable.containsKey(i)) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+        
+    }
+}
